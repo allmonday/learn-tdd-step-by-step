f0b807aa6c4886ba6a3fc464e022d132941a8cc9
tangkikodo <allmonday@126.com>
2015-05-25 07:48:34 +0800

    update

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    if request.method == 'POST':
+        new_item_text = request.POST.get('item_text', '')
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
